8fad5e2685757003ddb56b5612973e9214fb8b2a
wallbeach <thierry.thomann@gmail.com>
2022-01-08 11:44:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "[SETUP] Installing package dependencies ...";
 npm install;
 
 # Copy the _config.ts file and rename it to config.ts if it does not exist
-if [ ! -f "../Config.ts" ] ; then
+if [ ! -f "./src/_Config.ts" ] ; then
     echo "[SETUP] Config file not found! Copying ...";
     cp ./src/_Config.ts ./src/Config.ts;
 fi
